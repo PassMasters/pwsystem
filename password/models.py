a24bfefba91caf50fd50f0c0825766447ac30523
cs947939 <sufam5@outlook.com>
2023-02-07 23:32:55 +0000

    FAILURE!!!

--- password/models.py
+++ password/models.py
@@ -5,5 +5,5 @@ class Password(models.Model):
     Username = models.CharField(max_length=255, blank=True)
     Password = models.CharField(max_length=255, blank=True)
     TOTP = models.CharField(max_length=255, blank=True)
-    Atachment = models.FileField(validators=[validate_file_size])
-    Date_Created = models.DateField()
+    Atachment = models.FileField(validators=[validate_file_size], default='jkasdflajsdf')
+    Date_Created = models.DateField(default='2023-02-01')
