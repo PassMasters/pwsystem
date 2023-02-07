30d1e511ce399e9c6e8b122a13c0b3a4b2076c26
cs947939 <sufam5@outlook.com>
2023-02-07 19:04:57 +0000

    force an update

--- munchypw/settings.py
+++ munchypw/settings.py
@@ -36,9 +36,11 @@ INSTALLED_APPS = [
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
+    "django.contrib.sites",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "password"
+    "password",
+    "allauth",
 ]
 
 MIDDLEWARE = [
