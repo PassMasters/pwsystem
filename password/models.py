278cee693db3065bb44d72e0e8b9a1a22c67f773
cs947939 <sufam5@outlook.com>
2023-02-07 23:27:28 +0000

    added file size limit

--- password/models.py
+++ password/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+from .validators import validate_file_size
 # Create your models here.
 class Password(models.Model):
     Username = models.CharField(max_length=255, blank=True)
     Password = models.CharField(max_length=255, blank=True)
     TOTP = models.CharField(max_length=255, blank=True)
+    Atachment = models.FileField(validators=[validate_file_size])
---
+++ password/validators.py
@@ -0,0 +1,8 @@
+from django.core.exceptions import ValidationError
+def validate_file_size(value):
+    filesize= value.size
+    
+    if filesize > 5096:
+        raise ValidationError("You cannot upload file more than 1Mb")
+    else:
+        return value
