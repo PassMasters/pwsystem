c318cecb1dc80d7506f539e4a4513f5e06b2311b
cs947939 <sufam5@outlook.com>
2023-02-07 16:00:48 +0000

    1.0 protoptype

--- password/models.py
+++ password/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Password(models.Model):
+    Username = models.CharField(max_length=255, blank=True)
+    Password = models.CharField(max_length=255, blank=True)
+    TOTP = models.CharField(max_length=255, blank=True)
