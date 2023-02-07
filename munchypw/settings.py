70bb8a09479fa12a74480893189fa99cdd1a628b
cs947939 <sufam5@outlook.com>
2023-02-07 15:57:22 +0000

    wfsdfsdsfdfsdfsdf

--- munchypw/settings.py
+++ munchypw/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "munchypw.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ['BASE_DIR/templates'],
+        "DIRS": ['templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
