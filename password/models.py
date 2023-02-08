44f596b3e5c76fcc72afaac1bc2d2119bf7ccb7a
cs947939 <sufam5@outlook.com>
2023-02-08 20:54:02 +0000

    garbage

--- password/models.py
+++ password/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from encrypted_model_fields.fields import EncryptedCharField
 from .validators import validate_file_size
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Password(models.Model):
@@ -9,6 +10,11 @@ class Password(models.Model):
     TOTP = models.CharField(max_length=255, blank=True)
     Atachment = models.FileField(validators=[validate_file_size], default='jkasdflajsdf')
     Date_Created = models.DateField(default='2023-02-01')
+    
+   
+
+
+        
 
 
 class Secret(models.Model):
