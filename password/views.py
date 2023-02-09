956d83c9d5d13855c2642ff6e775db34c74535ca
cs947939 <sufam5@outlook.com>
2023-02-09 17:53:36 +0000

    dfhidsufhh

--- password/views.py
+++ password/views.py
@@ -9,7 +9,7 @@ from cryptography.fernet import Fernet
 def setup(request):
     if request.method == "POST":
         ekey = Encryption()
-
+        
         ekey.Owner = request.user
         ekey.save() 
 
@@ -22,17 +22,24 @@ def add(request):
     form = PasswordForm(request.POST)
     if request.method == "POST":
         
-        ekey = Encryption()
+        ekey = Encryption.objects.get(id=request.user)
+        token = ekey.key
+        yaa = Password()
         
-
-
         form = PasswordForm(request.POST)
         user = form.cleaned_data.get('Username')
         pw = form.cleaned_data.get('Password')
+        ks = Fernet(token)
+        newPassword = ks.encrypt(pw)
+        pw = newPassword
         TOTP = form.cleaned_data.get('TOTP')
         Atachment = form.cleaned_data('Atachment')
         
-
+        yaa.Username = user
+        yaa.Password = Password
+        yaa.TOTP = TOTP
+        yaa.Atachment = Atachment
+        yaa.save()
            
         return redirect('')
 
