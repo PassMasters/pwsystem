e0d00f6e8151a7fbce775abb2d5ef24d8b9bc702
cs947939 <sufam5@outlook.com>
2023-02-09 08:39:50 -0600

    Update models.py

--- password/models.py
+++ password/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from encrypted_model_fields.fields import EncryptedCharField
 from .validators import validate_file_size
 from django.contrib.auth.models import User
+import os
+import base64
+
+
 
 # Create your models here.
 class Password(models.Model):
@@ -11,7 +15,8 @@ class Password(models.Model):
     Atachment = models.FileField(validators=[validate_file_size], default='jkasdflajsdf')
     Date_Created = models.DateField(default='2023-02-01')
     
-   
+#class Encryption(models.Model):
+ #   Owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
         
