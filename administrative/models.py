4485275e103c93c0c10d5524e446c1bfec6cb893
coding986532 <97757073+coding986532@users.noreply.github.com>
2023-02-12 08:55:31 -0600

    Update models.py

--- administrative/models.py
+++ administrative/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+class Issue(models.Model):
+   Issue = models.CharField(max_length=280)
+   One_Detail = models.CharField(max_length=280)
+
+class Link(models.Model):
+    Title = models.CharField(max_length=280)
+    HyperLink = models.CharField(max_length=280)
+
+
 # Create your models here.
+#j
