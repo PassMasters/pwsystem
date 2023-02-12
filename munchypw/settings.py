a7f2ace1466d88b5e86762d39dcf85e49019a3f3
cs947939 <sufam5@outlook.com>
2023-02-12 13:04:45 -0800

    test

--- munchypw/settings.py
+++ munchypw/settings.py
@@ -160,10 +160,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-E_KEY = os.environ.get('E_KEY')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-FIELD_ENCRYPTION_KEY = 'g42sdzXCJWUpnsgdN5FKXuYHXz-GZmOcDqzigcziJXw='
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 ACCOUNT_ADAPTER = 'allauth_2fa.adapter.OTPAdapter'
 DEV_MODE = os.environ.get('DEV_MODE')
