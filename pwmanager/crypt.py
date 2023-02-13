a5075f9929c0b99feee4e6c8c124cfa86197ef0a
cs947939 <sufam5@outlook.com>
2023-02-13 14:28:36 -0600

    Update crypt.py

--- pwmanager/crypt.py
+++ pwmanager/crypt.py
@@ -5,7 +5,7 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 
-def encrypt (pin, input, output,salt):
+def encrypt (pin, input, salt):
         kdf = PBKDF2HMAC(algorithm=hashes.SHA256(),     length=32,  salt=salt,   iterations=100000, )
         key = base64.urlsafe_b64encode(kdf.derive(pin))
         e = Fernet(key)
@@ -15,7 +15,7 @@ def encrypt (pin, input, output,salt):
         output = s
         return output
 
-def decrypt (pin, input, output,salt):
+def decrypt (pin, input, salt):
         kdf = PBKDF2HMAC(algorithm=hashes.SHA256(),     length=32,  salt=salt,   iterations=100000, )
         key = base64.urlsafe_b64encode(kdf.derive(pin))
         e = Fernet(key)
