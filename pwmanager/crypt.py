1e24fb9451e943760d8ad3c304dd74853781708b
cs947939 <sufam5@outlook.com>
2023-02-13 20:25:48 +0000

    help2

--- pwmanager/crypt.py
+++ pwmanager/crypt.py
@@ -15,5 +15,14 @@ def encrypt (pin, input, output,salt):
         s = str(k, 'UTF-8')
         return s
 
+def decrypt (pin, input, output,salt):
+        kdf = PBKDF2HMAC(algorithm=hashes.SHA256(),     length=32,  salt=salt,   iterations=100000, )
+        key = base64.urlsafe_b64encode(kdf.derive(pin))
+        e = Fernet(key)
+        h = bytes(input, 'UTF-8')
+        k = e.encrypt(h)
+        s = str(k, 'UTF-8')
+        return s
+
         
         
