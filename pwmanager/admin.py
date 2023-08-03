e700937838c31629b877937ddb41d1b04a635d6e
cs947939 <sufam5@outlook.com>
2023-08-02 22:51:39 -0500

    Setting up custom user

--- pwmanager/admin.py
+++ pwmanager/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
-from .models import Password, Secret, Encryption
+from .models import Password, Secret, Encryption, PwUser
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
+
+
+
+# Define an inline admin descriptor for Employee model
+# which acts a bit like a singleton
+class EmployeeInline(admin.StackedInline):
+    model = PwUser
+    can_delete = False
+    verbose_name_plural = "PwUsers"
+
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = [EmployeeInline]
+
+
+# Re-register UserAdmin
+admin.site.unregister(PwUser)
+admin.site.register(PwUser, UserAdmin)
 admin.site.register(Password)
 admin.site.register(Secret)
 admin.site.register(Encryption)
