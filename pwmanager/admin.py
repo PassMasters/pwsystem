887602e6c89d840ca8ce9f3c199a100ddc11d357
cs947939 <sufam5@outlook.com>
2023-08-03 13:11:32 +0000

    fixing

--- pwmanager/admin.py
+++ pwmanager/admin.py
@@ -9,7 +9,7 @@ from django.contrib.auth.models import User
 
 # Define an inline admin descriptor for Employee model
 # which acts a bit like a singleton
-class EmployeeInline(admin.StackedInline):
+class PwUserInline(admin.StackedInline):
     model = PwUser
     can_delete = False
     verbose_name_plural = "PwUsers"
@@ -17,11 +17,11 @@ class EmployeeInline(admin.StackedInline):
 
 # Define a new User admin
 class UserAdmin(BaseUserAdmin):
-    inlines = [EmployeeInline]
+    inlines = [PwUserInline]
 
 
 # Re-register UserAdmin
-admin.site.unregister(PwUser)
+
 admin.site.register(PwUser, UserAdmin)
 admin.site.register(Password)
 admin.site.register(Secret)
