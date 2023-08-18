147b4694670b10284898915f68472fc1cb019095
coding2583571 <97715381+coding2583571@users.noreply.github.com>
2023-08-17 21:00:18 -0700

    this is actual garbage

--- administrative/models.py
+++ administrative/models.py
@@ -2,7 +2,11 @@ from django.db import models
 
 class Issue(models.Model):
    Issue = models.CharField(max_length=280)
+   Date = models.DateField(blank=True)
    One_Detail = models.CharField(max_length=280, blank=True)
+   Second_Detail = models.CharField(max_length=280, blank=True)
+   Third_Detail = models.CharField(max_length=280, blank=True)
+
 
 class Link(models.Model):
     Title = models.CharField(max_length=280)
--- administrative/views.py
+++ administrative/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Link, Issue
+
+def dev_home(request):
+        issues = Issue.objects.all().order_by('-Date')
+        links = Link.objects.all()
+        return render(request, "version.html",  {'issues':issues,'links':links})
+
 
 # Create your views here.
