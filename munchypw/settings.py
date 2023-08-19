19b0108109c5296dcaef15fcc153a004f6971e22
coding2583571 <97715381+coding2583571@users.noreply.github.com>
2023-08-19 06:29:06 -0700

    fixing possible mistake

--- munchypw/settings.py
+++ munchypw/settings.py
@@ -136,13 +136,13 @@ USE_I18N = True
 USE_TZ = True
 
 AUTHENTICATION_BACKENDS = [
-    ...
+   ''' ...
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
 
     # `allauth` specific authentication methods, such as login by e-mail
     'allauth.account.auth_backends.AuthenticationBackend',
-    ...
+    ... '''
 ]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
