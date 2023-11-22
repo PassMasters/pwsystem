7ba841c70ca6e32f3ee504d21ebe2c259a4dac27
cs947939 <sufam5@outlook.com>
2023-11-22 10:02:18 -0600

    addng more git ignore

--- lisenceclient/ConsoleApp1/Program.cs
+++ lisenceclient/ConsoleApp1/Program.cs
@@ -7,37 +7,75 @@ using Newtonsoft.Json;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using Newtonsoft.Json.Linq;
-Console.WriteLine("Enter your license key:");
-string licenseKey = Console.ReadLine();
+using Microsoft.Win32;
+string registryKeyPath = "SOFTWARE\\YourCompany\\YourApp";
+string valueName = "RandomNumber";
 
-if (string.IsNullOrWhiteSpace(licenseKey))
+bool keyexsits = RandomNumberReg(registryKeyPath, valueName, out string randomNumber);
+if (keyexsits != true)
 {
-    Console.WriteLine("License key cannot be empty.");
-    return;
-}
+    Console.WriteLine("Enter your license key:");
+    string licenseKey = Console.ReadLine();
 
-// Replace with your actual API endpoint
-string apiUrl = "http://127.0.0.1:8000/key/TokenRequest/";
+    if (string.IsNullOrWhiteSpace(licenseKey))
+    {
+        Console.WriteLine("License key cannot be empty.");
+        return;
+    }
+    string apiUrl = "http://127.0.0.1:8000/key/TokenRequest/";
 
-// Send the license key to the server
-string response = await SendLicenseKey(apiUrl, licenseKey);
-string decodedToken = ExtractTokenFromJsonResponse(response);
-// Interpret the server response
-Console.WriteLine("Server response:");
-Console.WriteLine(response);
-Console.WriteLine("decoding:");
-DecodeJwt(decodedToken);
-Console.WriteLine("validating lisence key");
-var results = ValidateJwt(decodedToken, "OIDFJIODSFJIODSFJIU(WFHOISDF903248uweriy87345ureiyrtb965258752475201258525475sduri6838ejmfiuvmknmeujdjedjdjjdjdjdjd)");
+    // Send the license key to the server
+    string response = await SendLicenseKey(apiUrl, licenseKey);
+    string decodedToken = ExtractTokenFromJsonResponse(response);
+    // Interpret the server response
+    Console.WriteLine("Server response:");
+    Console.WriteLine(response);
+    Console.WriteLine("decoding:");
+    DecodeJwt(decodedToken);
+    Console.WriteLine("validating lisence key");
+    var results = ValidateJwt(decodedToken, "OIDFJIODSFJIODSFJIU(WFHOISDF903248uweriy87345ureiyrtb965258752475201258525475sduri6838ejmfiuvmknmeujdjedjdjjdjdjdjd)");
 
-if (results != null)
-{
-    Console.WriteLine("License key is valid.");
+    if (results != null)
+    {
+        Console.WriteLine("License key is valid.");
+    }
+    else
+    {
+        Console.WriteLine("License key is invalid.");
+    }
 }
 else
 {
-    Console.WriteLine("License key is invalid.");
+    Console.WriteLine("SystemManagement");
+    Console.WriteLine("press 1 for Deactivate");
+    Console.WriteLine("press 2 for device registration");
+    Console.WriteLine("press 3 to download your passwords from server");
+    Console.WriteLine("press 4 to chnage lisence key");
+    Console.WriteLine("press 5 to exit");
+
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        default:
+            break;
+    }
 }
+
+}
+
+
+// Replace with your actual API endpoint
+
 static string ExtractTokenFromJsonResponse(string jsonResponse)
 {
     // Parse the JSON response
@@ -75,6 +113,9 @@ static ClaimsPrincipal ValidateJwt(string jwt, string secretKey)
         ClaimsPrincipal claimsPrincipal = handler.ValidateToken(jwt, tokenValidationParameters, out securityToken);
 
         // Additional validation logic can be added if needed
+        int randomNumber = int.Parse(claimsPrincipal.FindFirst("random_number")?.Value);
+        string data = randomNumber.ToString();
+        StoreRandomNumberInRegistry(data, "rand");
         Console.WriteLine("JWT is valid!");
         return claimsPrincipal;
         
@@ -129,4 +170,39 @@ async Task<string> SendLicenseKey(string apiUrl, string licenseKey)
             return $"Error: {response.StatusCode}";
         }
     }
+}
+static void StoreRandomNumberInRegistry(string data, string keyname)
+{
+    // Specify the registry key path (you can customize this)
+    string registryKeyPath = "SOFTWARE\\Passmasters\\PWsystem";
+
+    // Open or create the registry key
+    using (var registryKey = Registry.CurrentUser.CreateSubKey(registryKeyPath))
+    {
+        // Store the random number in the registry
+        registryKey?.SetValue(keyname, data);
+    }
+}
+static bool RandomNumberReg(string keyPath, string valueName, out string randomNumber)
+{
+    // Open the registry key for reading
+    using (var registryKey = Registry.CurrentUser.OpenSubKey(keyPath))
+    {
+        // Check if the registry key exists
+        if (registryKey != null)
+        {
+            // Check if the value exists in the registry
+            if (registryKey.GetValue(valueName) is string value)
+            {
+                // The value exists, assign it to 'randomNumber' and return true
+                randomNumber = value;
+                return true;
+            }
+        }
+    }
+
+    // The value does not exist or there was an error, assign a default value to 'randomNumber' and return false
+    randomNumber =" 0";
+    return false;
+}
 }
