3a7c229f0d39b0150ffc0bcc5f5d8d32491f1e42
cs947939 <sufam5@outlook.com>
2024-01-05 17:49:14 -0600

    all issues are fixed

--- munchypw/settings.py
+++ munchypw/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
- #'allauth.account.middleware.AccountMiddleware',
+     'allauth.account.middleware.AccountMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
      'whitenoise.middleware.WhiteNoiseMiddleware',
